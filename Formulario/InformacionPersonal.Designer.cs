b8ab7ce2006d5d97fee17e26c5b3a70f1f2aedff
Juan José Echavarria Castaño <juanjoseechavarriacastano@gmail.com>
2025-03-11 18:28:02 -0500

    Agregar archivos de proyecto.

---
+++ Formulario/InformacionPersonal.Designer.cs
@@ -0,0 +1,192 @@
+﻿using System.Windows.Forms;
+
+namespace Formulario
+{
+    partial class frmInformacionPersonal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInformacionPersonal));
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblDocumento = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblInformacionPersonal = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.gbInformacionPersonal = new System.Windows.Forms.GroupBox();
+            this.gbContacto = new System.Windows.Forms.GroupBox();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.gbInformacionPersonal.SuspendLayout();
+            this.gbContacto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtDocumento
+            // 
+            resources.ApplyResources(this.txtDocumento, "txtDocumento");
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDocumento_KeyPress);
+            // 
+            // txtTelefono
+            // 
+            resources.ApplyResources(this.txtTelefono, "txtTelefono");
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTelefono_KeyPress);
+            // 
+            // lblDocumento
+            // 
+            resources.ApplyResources(this.lblDocumento, "lblDocumento");
+            this.lblDocumento.Name = "lblDocumento";
+            // 
+            // lblNombre
+            // 
+            resources.ApplyResources(this.lblNombre, "lblNombre");
+            this.lblNombre.Name = "lblNombre";
+            // 
+            // lblApellido
+            // 
+            resources.ApplyResources(this.lblApellido, "lblApellido");
+            this.lblApellido.Name = "lblApellido";
+            // 
+            // lblTelefono
+            // 
+            resources.ApplyResources(this.lblTelefono, "lblTelefono");
+            this.lblTelefono.Name = "lblTelefono";
+            // 
+            // lblInformacionPersonal
+            // 
+            resources.ApplyResources(this.lblInformacionPersonal, "lblInformacionPersonal");
+            this.lblInformacionPersonal.Name = "lblInformacionPersonal";
+            // 
+            // txtNombre
+            // 
+            resources.ApplyResources(this.txtNombre, "txtNombre");
+            this.txtNombre.Name = "txtNombre";
+            // 
+            // txtApellido
+            // 
+            resources.ApplyResources(this.txtApellido, "txtApellido");
+            this.txtApellido.Name = "txtApellido";
+            // 
+            // txtCorreo
+            // 
+            resources.ApplyResources(this.txtCorreo, "txtCorreo");
+            this.txtCorreo.Name = "txtCorreo";
+            // 
+            // btnAceptar
+            // 
+            resources.ApplyResources(this.btnAceptar, "btnAceptar");
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            resources.ApplyResources(this.btnCancelar, "btnCancelar");
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // gbInformacionPersonal
+            // 
+            this.gbInformacionPersonal.Controls.Add(this.txtDocumento);
+            this.gbInformacionPersonal.Controls.Add(this.lblDocumento);
+            this.gbInformacionPersonal.Controls.Add(this.lblNombre);
+            this.gbInformacionPersonal.Controls.Add(this.lblApellido);
+            this.gbInformacionPersonal.Controls.Add(this.txtNombre);
+            this.gbInformacionPersonal.Controls.Add(this.txtApellido);
+            resources.ApplyResources(this.gbInformacionPersonal, "gbInformacionPersonal");
+            this.gbInformacionPersonal.Name = "gbInformacionPersonal";
+            this.gbInformacionPersonal.TabStop = false;
+            // 
+            // gbContacto
+            // 
+            this.gbContacto.Controls.Add(this.lblCorreo);
+            this.gbContacto.Controls.Add(this.lblTelefono);
+            this.gbContacto.Controls.Add(this.txtCorreo);
+            this.gbContacto.Controls.Add(this.txtTelefono);
+            resources.ApplyResources(this.gbContacto, "gbContacto");
+            this.gbContacto.Name = "gbContacto";
+            this.gbContacto.TabStop = false;
+            // 
+            // lblCorreo
+            // 
+            resources.ApplyResources(this.lblCorreo, "lblCorreo");
+            this.lblCorreo.Name = "lblCorreo";
+            // 
+            // btnLimpiar
+            // 
+            resources.ApplyResources(this.btnLimpiar, "btnLimpiar");
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // frmInformacionPersonal
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.gbContacto);
+            this.Controls.Add(this.gbInformacionPersonal);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.lblInformacionPersonal);
+            this.Name = "frmInformacionPersonal";
+            this.gbInformacionPersonal.ResumeLayout(false);
+            this.gbInformacionPersonal.PerformLayout();
+            this.gbContacto.ResumeLayout(false);
+            this.gbContacto.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDocumento;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblInformacionPersonal;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox gbInformacionPersonal;
+        private System.Windows.Forms.GroupBox gbContacto;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Formulario/InformacionPersonal.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class frmInformacionPersonal: Form
+    {
+        public frmInformacionPersonal()
+        {
+            InitializeComponent();
+
+        }
+
+        private void LimpiarCampos()
+        {
+            txtDocumento.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtCorreo.Clear();
+            txtTelefono.Clear();
+
+            txtDocumento.Focus();
+        }
+
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDocumento.Text) ||
+                string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                string.IsNullOrWhiteSpace(txtApellido.Text) ||
+                string.IsNullOrWhiteSpace(txtCorreo.Text) ||
+                string.IsNullOrWhiteSpace(txtTelefono.Text))
+            {
+                MessageBox.Show("Por favor, complete todos los campos antes de continuar.", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult resultado = MessageBox.Show("Información guardada correctamente.\n¿Desea registrar otro usuario?","Éxito", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.Yes)
+            {
+                LimpiarCampos();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+
+        private void txtDocumento_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if ((e.KeyChar < 48 || e.KeyChar > 57) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
